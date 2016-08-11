d81c77a0523b294d6c6e4d651b59ba8031305dc4
quisnothing <quisnothing@gmail.com>
2016-08-11 22:30:00 +0800

    添加当前位置的显示。。。。

--- app/src/main/java/com/example/baidumaptest/MainActivity.java
+++ app/src/main/java/com/example/baidumaptest/MainActivity.java
@@ -1,25 +1,103 @@
 package com.example.baidumaptest;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
+    private BaiduMap baiduMap;
+    private LocationManager locationManager;
+    private String provider;
+    private boolean isFirstLocate=true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         mapView=(MapView)findViewById(R.id.map_view);
+        baiduMap=mapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
+        locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        //获取所有可用的位置提供器
+        List<String> providerList=locationManager.getProviders(true);
+        if(providerList.contains(LocationManager.GPS_PROVIDER)){
+            //查找到有GPS这个为之提供器
+            provider=LocationManager.GPS_PROVIDER;
+        }else if(providerList.contains(LocationManager.NETWORK_PROVIDER)){
+            provider=LocationManager.NETWORK_PROVIDER;
+        }else{
+            Toast.makeText(this,"No location provider to use",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Location location=locationManager.getLastKnownLocation(provider);
+        if(location!=null){
+            navigateTo(location);
+        }
+        locationManager.requestLocationUpdates(provider,5000,1,locationListener);
+    }
+    private void navigateTo(Location location){
+        if(isFirstLocate){
+            LatLng ll=new LatLng(location.getLatitude(),location.getLongitude());
+            MapStatusUpdate update= MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            update=MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            isFirstLocate=false;
+        }
+        MyLocationData.Builder locationBuilder=new MyLocationData.Builder();
+        locationBuilder.latitude(location.getLatitude());
+        locationBuilder.longitude(location.getLongitude());
+        MyLocationData locationData=locationBuilder.build();
+        baiduMap.setMyLocationData(locationData);
     }
 
+    LocationListener locationListener=new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(location!=null){
+                locationManager.removeUpdates(locationListener);
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
     protected void onDestroy(){
         super.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
         mapView.onDestroy();
+        if(locationManager!=null){
+            //关闭程序时将监听器移除
+            locationManager.removeUpdates(locationListener);
+        }
     }
 
     protected void onPause(){
